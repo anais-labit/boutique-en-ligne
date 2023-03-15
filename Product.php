aa3678779492c3be26481152dad8941bc0ce2423
anais-labit <anais.labit@gmail.com>
2023-03-15 17:55:52 +0100

    v1 autocomplétion

---
+++ Product.php
@@ -0,0 +1,36 @@
+<?php
+
+class Product
+{
+    private $PDO;
+    private ?string $name;
+    private ?string $id_producteur;
+
+    public function __construct() {
+        $DB_DSN = 'mysql:host=localhost; dbname=boutique-en-ligne';
+        $DB_USER = 'root';
+        $DB_PASS = '';
+
+        try {
+            $this->PDO = new PDO($DB_DSN, $DB_USER, $DB_PASS);
+            // echo 'Connexion établie';
+            // return TRUE;
+        } catch (PDOException $e) {
+            die('ERREUR :' . $e->getMessage());
+        }
+    }
+
+    public function catchProductInfos($word)
+    {
+        $this->PDO;
+        $req = "SELECT * FROM produits WHERE nom LIKE '$word%'";
+        
+        $catchProduct = $this->PDO->prepare($req);
+        $catchProduct->execute();
+        $results = $catchProduct->fetchAll(PDO::FETCH_ASSOC);
+        $json = json_encode($results);
+        echo $json;
+    }
+}
+
+?>
---
+++ auto_complete.js
@@ -0,0 +1,16 @@
+const field = document.querySelector("#field");
+
+
+field.addEventListener("keyup", async (event) => {
+  event.preventDefault();
+  let filledField = field.value;
+//   console.log(filledField);
+
+  if (filledField.length != 0) {
+    fetch("../Controller/search.php?field=" + filledField)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+      }); 
+  } 
+});
