10acb5b52d27368d3c41f9270127c4a5653d1efe
alexandre-aloesode <alexandre.aloesode@laplateforme.io>
2023-04-05 12:56:27 +0200

    modif scripts panier et ajout gitignore vendor/

--- src/Controller/cart.js
+++ src/Controller/cart.js
@@ -1,5 +1,13 @@
 
 const productsDiv =  document.querySelector("#productsDiv");
+const cartIcon = document.querySelector("#cartIcon");
+const headerCartDiv = document.querySelector("#headerCartDiv");
+const cartContainer = document.querySelector("#cartContainer");
+
+headerCartDiv.style.display = "none";
+
+cartContainer.addEventListener("mouseover", showHeaderCart);
+cartContainer.addEventListener("mouseout", hideHeaderCart);
 
 async function displayAllProducts() {
 
@@ -17,7 +25,6 @@ async function displayAllProducts() {
     const allProducts = await fetch("../src/Routes/product_display.php", requestAllProductsOptions)
 
     const productList = await allProducts.json()
-    console.log(productList);
 
     for(let i in productList) {
 
@@ -46,8 +53,7 @@ async function displayAllProducts() {
         card.appendChild(addCartButton);
 
         productsDiv.appendChild(card);
-
-        
+      
     }
     
 }
@@ -71,8 +77,85 @@ async function addCart() {
 
     if(cartUpdate.success == "true") {
 
-        location.reload();
+        displayHeaderCart();
+        showCartNumber();
     }
 }
 
+
+async function fetchHeaderCart() {
+
+   const fetchHeaderCartForm = new FormData();
+
+   fetchHeaderCartForm.append("displayHeaderCart", "displayHeaderCart");
+
+   const requestfetchHeaderCart = {
+
+        method:"POST",
+        body: fetchHeaderCartForm
+   }
+
+   const searchHeaderCart = await fetch("../src/Routes/cart_management.php", requestfetchHeaderCart);
+
+   const result = await searchHeaderCart.json();
+
+   console.log(result[0]);
+
+   return result
+
+}
+
+async function displayHeaderCart() {
+
+    const cartList = await fetchHeaderCart();
+
+    headerCartDiv.innerHTML = "";
+
+    for(let i in cartList.result) {
+
+        const cartLine = document.createElement("p");
+
+        cartLine.innerHTML= cartList.result[i];
+
+        headerCartDiv.appendChild(cartLine);
+
+       
+
+   }
+
+   const goToCart = document.createElement("a");
+   goToCart.setAttribute("href", "cart.php");
+   
+   const goToCartButton = document.createElement("button");
+   goToCartButton.innerHTML = "Voir mon panier";
+
+   goToCart.appendChild(goToCartButton);
+   headerCartDiv.appendChild(goToCart);
+}
+
+function showHeaderCart() {
+
+    headerCartDiv.style.display = "block";
+    // headerCartDiv.style.position = "fixed"
+
+}
+
+function hideHeaderCart() {
+    headerCartDiv.style.display = "none";
+
+}
+
+async function showCartNumber() {
+
+    const cartNumber = await fetchHeaderCart();
+    cartIcon.innerHTML = "";
+    cartIcon.innerHTML = cartIcon.innerHTML + " " + cartNumber.count;
+
+}
+
 displayAllProducts();
+showCartNumber();
+displayHeaderCart();
+
+
+// cartIcon.innerHTML = cartIcon.innerHTML + 1
