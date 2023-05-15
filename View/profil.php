170066513c24697e2adcdc521c9e49df13b70262
anais-labit <anais.labit@gmail.com>
2023-05-15 14:03:41 +0200

    css page profil

--- View/profil.php
+++ View/profil.php
@@ -29,19 +29,19 @@ if (session_id() == "") session_start();
     <?php require_once 'header.php';
     $currentAvatar = $_SESSION['user']->getAvatar(); ?>
 
-    <form action="profil.php" method="POST" id="updateForm" enctype="multipart/form-data">
+    <div class="profil-page">
+        <h2>Gestion de profil</h2>
+        <form action="profil.php" method="POST" id="updateForm" enctype="multipart/form-data">
 
-        <?php if ($_SESSION['user']->getType() == 1) : ?>
-            <label>Particulier</label>
-        <?php elseif ($_SESSION['user']->getType() == 2) : ?>
-            <label>Entreprise</label>
-        <?php endif ?>
+            <?php if ($_SESSION['user']->getType() == 1) : ?>
+                <label>Particulier</label>
+            <?php elseif ($_SESSION['user']->getType() == 2) : ?>
+                <label>Entreprise</label>
+            <?php endif ?>
 
-
-        <!-- <label class="updateLabels" id="updateLabelAvatar" for="updateLabelAvatar">Avatar</label> -->
-        <div class="avatar" id="avatarContainer"><img src="<?= './assets/images/avatars/avatar' . $currentAvatar . '.png' ?>" width="60" height="60" alt="avatar"></div>
-
-        <!-- <input type=" radio" class="updateLabels" name="avatar" id="avatar1" value="1">
+            <!-- <label class="updateLabels" id="updateLabelAvatar" for="updateLabelAvatar">Avatar</label> -->
+            <div class="avatar" id="avatarContainer"><img src="<?= './assets/images/avatars/avatar' . $currentAvatar . '.png' ?>" width="60" height="60" alt="avatar"></div>
+            <!-- <input type=" radio" class="updateLabels" name="avatar" id="avatar1" value="1">
             <img class="avatar" alt="avatar" src="./images/avatar1.png" width="60" height="60" />
             <input type="radio" class="updateLabels" name="avatar" id="avatar2" value="2">
             <img class="avatar" alt="avatar" src="./images/avatar2.png" width="60" height="60" />
@@ -51,47 +51,38 @@ if (session_id() == "") session_start();
             <img class="avatar" alt="avatar" src="./images/avatar4.png" width="60" height="60" />
             <input type="radio" class="updateLabels" name="avatar" id="avatar5" value="5">
             <img class="avatar" alt="avatar" src="./images/avatar5.png" width="60" height="60" /> -->
-        <?php if ($_SESSION['user']->getType() == 1) : ?>
-            <label class="updateLabels" id="updateLabelFirstName" for="updateFirstName">Prénom</label>
-            <input type="text" name="updateFirstName" class="loginInputs" id="updateFirstName" value="<?= $_SESSION['user']->getFirstName() ?>">
+            <?php if ($_SESSION['user']->getType() == 1) : ?>
+                <label class="updateLabels" id="updateLabelFirstName" for="updateFirstName">Prénom</label>
+                <input type="text" name="updateFirstName" class="loginInputs" id="updateFirstName" value="<?= $_SESSION['user']->getFirstName() ?>">
 
-            <label class="updateLabels" id="updateLabelLastName" for="updateLastName">Nom</label>
-            <input type="text" name="updateLastName" class="loginInputs" id="updateLastName" value="<?= $_SESSION['user']->getLastName() ?>">
-        <?php elseif ($_SESSION['user']->getType() == 2) : ?>
-            <label class="updateLabels" id="updateLabelCompany" for="updateCompany">Raison Sociale</label>
-            <input type="text" name="updateCompany" class="loginInputs" id="updateCompany" value="<?= $_SESSION['user']->getCompany() ?>">
-        <?php endif ?>
+                <label class="updateLabels" id="updateLabelLastName" for="updateLastName">Nom</label>
+                <input type="text" name="updateLastName" class="loginInputs" id="updateLastName" value="<?= $_SESSION['user']->getLastName() ?>">
+            <?php elseif ($_SESSION['user']->getType() == 2) : ?>
+                <label class="updateLabels" id="updateLabelCompany" for="updateCompany">Raison Sociale</label>
+                <input type="text" name="updateCompany" class="loginInputs" id="updateCompany" value="<?= $_SESSION['user']->getCompany() ?>">
+            <?php endif ?>
 
-        <label class="updateLabels" for="updateEmail">Email</label>
-        <input type="text" name="updateEmail" class="loginInputs" id="updateEmail" value="<?= $_SESSION['user']->getEmail() ?>">
+            <label class="updateLabels" for="updateEmail">Email</label>
+            <input type="text" name="updateEmail" class="loginInputs" id="updateEmail" value="<?= $_SESSION['user']->getEmail() ?>">
+            <label class="updateLabels" for="updateAddress">Adresse</label>
+            <input type="text" name="updateAddress" class="loginInputs" id="updateAddress" value="<?= $_SESSION['user']->getAddress() ?>">
+            <label class="updateLabels" for="updateZipCode">Code Postal</label>
+            <input type="number" name="updateZipCode" class="loginInputs" id="updateZipCode" value="<?= $_SESSION['user']->getZipCode() ?>">
+            <label class="updateLabels" for="updateCity">Ville</label>
+            <input type="text" name="updateCity" class="loginInputs" id="updateCity" value="<?= $_SESSION['user']->getCity() ?>">
+            <label class="updateLabels" for="updatePassword">Mot de Passe</label>
+            <input type="password" name="updatePassword" class="loginInputs" id="updatePassword">
+            <label class="updateLabels" for="updateConfirmPassword">Confirmez votre mot de passe</label>
+            <input type="password" name="updateConfirmPassword" class="loginInputs" id="updateConfirmPassword">
+            <label for="confirmOldPassword">Saisissez votre ancien ancien mot de passe pour valider</label>
+            <input type="password" name="confirmOldPassword" class="loginInputs" id="confirmOldPassword">
 
-        <label class="updateLabels" for="updateAddress">Adresse</label>
-        <input type="text" name="updateAddress" class="loginInputs" id="updateAddress" value="<?= $_SESSION['user']->getAddress() ?>">
-
-        <label class="updateLabels" for="updateZipCode">Code Postal</label>
-        <input type="number" name="updateZipCode" class="loginInputs" id="updateZipCode" value="<?= $_SESSION['user']->getZipCode() ?>">
-
-        <label class="updateLabels" for="updateCity">Ville</label>
-        <input type="text" name="updateCity" class="loginInputs" id="updateCity" value="<?= $_SESSION['user']->getCity() ?>">
-
-        <label class="updateLabels" for="updatePassword">Mot de Passe</label>
-        <input type="password" name="updatePassword" class="loginInputs" id="updatePassword">
-
-        <label class="updateLabels" for="updateConfirmPassword">Confirmez votre mot de passe</label>
-        <input type="password" name="updateConfirmPassword" class="loginInputs" id="updateConfirmPassword">
-
-        <label for="confirmOldPassword">Saisissez votre ancien ancien mot de passe</label>
-        <input type="password" name="confirmOldPassword" class="loginInputs" id="confirmOldPassword">
-
-        <button type="submit" id="updateButton">Mettre à jour</button>
-        <button type="submit" id="deleteButton">Supprimer votre compte</button>
-
-
-    </form>
-
-
-
-    </form>
+            <div class="profilBtnContainer">
+                <button type="submit" class="profileBtn" id="updateButton">Mettre à jour</button>
+                <button type="submit" class="profileBtn" id="deleteButton">Supprimer votre compte</button>
+            </div>
+        </form>
+    </div>
 
 </body>
 
